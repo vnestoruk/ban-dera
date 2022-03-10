6808cba0ea6f3c9dbaa09aefe0ddfbfbfee022fa
Vasyl Nestoruk <nestoruk93@gmail.com>
2022-03-10 22:56:30 +0200

    Http from https blocked requests fixed.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
 
     <title>BAN-dera</title>
 
