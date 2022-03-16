956fd7e1754532a528b74f7ef5a323bdca697cb3
Eugene Esca <scsmash3r@gmail.com>
2022-03-16 21:54:27 +0200

    Some more targets + minor codestyle works (#12)
    * Nightmode for offcanvas list fix
    
    * Nightmode for modals and monobank logo
    
    * npx note for devs who work with laragon or wamp
    
    * Fix CSP policy on local env
    
    * Apply standard codestyle (.editorconf + formatter)
    
    * Prettify targets list + instructions for adding
    
    * Add koronapay targets
    
    * Add healthsystem targets
    
    * Add movie/cinema targets
    
    * Add online banking targets
    
    * Add digicert sign service targets
    
    * Add rosteh target
    
    * Add NSPK targets
    
    * Remove wikimapia source
    
    * Add spark, sbis, casebook targets

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,9 +1,12 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    @env('production')
     <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+    @endenv
 
     <title>BAN-dera</title>
 
@@ -13,14 +16,16 @@
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-E2JP8HENB3"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
         gtag('js', new Date());
 
         gtag('config', 'G-E2JP8HENB3');
@@ -103,6 +108,7 @@
         body[data-theme="night"] .modal .modal-body {
             color: var(--color-text) !important;
         }
+
         body[data-theme="night"] .modal .modal-header {
             border-bottom: 1px solid var(--color-faded);
         }
@@ -140,26 +146,30 @@
             width: 50%;
         }
 
-        @media screen and (max-width: 768px){
+        @media screen and (max-width: 768px) {
             .offcanvas {
                 width: 100%;
             }
         }
-
     </style>
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-E2JP8HENB3"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
         gtag('js', new Date());
 
         gtag('config', 'G-E2JP8HENB3');
     </script>
 </head>
+
 <body>
-<div id="app"></div>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-<script type="application/javascript" src="js/app.js"></script>
+    <div id="app"></div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script type="application/javascript" src="js/app.js"></script>
 </body>
+
 </html>
