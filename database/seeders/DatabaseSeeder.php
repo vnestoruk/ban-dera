78d5c5d9e4007a6e6b257c2b124ad4d16ac04637
vnestoruk <nestoruk93@gmail.com>
2022-06-04 21:13:16 +0300

    DB seeder improvements;

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            TargetSeeder::class
+            CategorySeeder::class,
+            TargetSeeder::class,
+            QuoteSeeder::class
         ]);
     }
 }
